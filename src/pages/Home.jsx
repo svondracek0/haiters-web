55227dd1c7ee6a37d53546cfefadc7ed85c2aa23
Stepan Vondracek <svondracek@mail.muni.cz>
2025-12-23 13:57:37 +0100

    create develop

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Sectors from '../components/Sectors';
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <Sectors />
+        </>
+    );
+};
+
+export default Home;
