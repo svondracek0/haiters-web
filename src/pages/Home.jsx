eab3d26aabee2ccd635012dbf11726216ca22ac8
svondracek0 <svondracek@mail.muni.cz>
2026-01-17 23:15:04 +0100

    Develop to main (#2)
    * create develop
    
    * Change color scheme
    
    * Simplify the page and design
    
    * simplify design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import Sectors from '../components/Sectors';
 
 const Home = () => {
     return (
         <>
             <Hero />
-            <Sectors />
         </>
     );
 };
